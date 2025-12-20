4f85a5fd4c394306981fe5f1ca81556171816bb9
Khanon Curtis <khanon.boyd@gmail.com>
2025-12-20 11:15:14 -0700

    update rustup.sh

--- supplement/rustup.sh
+++ supplement/rustup.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
-sudo pacman -S --noconfirm rustup
+if command -v rustup >/dev/null 2>&1; then
+    echo "⚠ rustup is already installed. Skipping."
+    exit 0
+fi
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
