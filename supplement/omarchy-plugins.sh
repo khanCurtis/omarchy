aaed625038ba1c86c0171e232c1127cd8f02ddb4
Khanon Curtis <khanon.boyd@gmail.com>
2026-09-04 12:25:23 -0600

    add plugins

---
+++ supplement/omarchy-plugins.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+omarchy plugin add https://github.com/fross100/omaplug.git --enable
+omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable
+omarchy plugin add https://github.com/SirJul1337/omarchy-lock-explorer.git --enable
+omarchy plugin add https://github.com/bobby-nicholas/omaland.git --enable
+omarchy plugin add https://github.com/akitaonrails/ai-usagebar.git --enable
+omarchy plugin add https://github.com/jankeesvw/omarchy-notification-center.git --enable
