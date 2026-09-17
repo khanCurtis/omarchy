986fb8b00d49ba6b79e549e0d353e5f82a361255
Khanon Curtis <khanon.boyd@gmail.com>
2026-09-17 13:44:17 -0600

    add more plugins

--- supplement/omarchy-plugins.sh
+++ supplement/omarchy-plugins.sh
@@ -10,3 +10,6 @@ omarchy pkg aur add ai-usagebar-bin
 omarchy plugin add https://github.com/jankeesvw/omarchy-notification-center.git --enable
 omarchy plugin add https://github.com/DanSmith888/omarchy-momentum4.git --enable
 omarchy plugin add https://github.com/thinklinux/omacursorshake.git --enable
+omarchy plugin add https://github.com/MASH2RAFI/omarchy-kronos.git --enable
+omarchy plugin add https://github.com/alexanderpuschkinberlin/omarchy-keyboard-backlight.git --enable
+omarchy plugin add https://github.com/ArtMoreno/omarchy-trackpoint.git --enable
